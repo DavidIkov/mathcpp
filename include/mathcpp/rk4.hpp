a2b47a89379fc257bf3ca000eda164f9a9891cad
DavidIkov <postnikovdavid@yandex.ru>
2025-10-01 23:50:28 +0300

    Constexpr modifications for rk4

--- include/mathcpp/rk4.hpp
+++ include/mathcpp/rk4.hpp
@@ -8,14 +8,14 @@ template <typename TimeT, typename ValueT, typename ContextT>
 class RK4 {
 public:
     template <typename TimeTT, typename ValueTT, typename ContextTT>
-    RK4(TimeTT&& t0, ValueTT&& y0, ContextTT&& context,
-        ValueT (*f)(TimeT const&, ValueT const&, ContextT const&))
+    constexpr RK4(TimeTT&& t0, ValueTT&& y0, ContextTT&& context,
+                  ValueT (*f)(TimeT const&, ValueT const&, ContextT const&))
         : t_(std::forward<TimeTT&&>(t0)),
           y_(std::forward<ValueTT&&>(y0)),
           context_(std::forward<ContextTT&&>(context)),
           f_(f) {}
 
-    ValueT const& Step(TimeT const& dt) {
+    constexpr ValueT const& Step(TimeT const& dt) {
         ValueT k1 = f_(t_, y_, context_);
         ValueT k2 = f_(t_ + dt / 2, y_ + dt * k1 / 2, context_);
         ValueT k3 = f_(t_ + dt / 2, y_ + dt * k2 / 2, context_);
@@ -25,9 +25,9 @@ public:
         return y_;
     }
 
-    [[nodiscard]] TimeT const& GetT() const { return t_; }
-    [[nodiscard]] ValueT const& GetY() const { return y_; }
-    [[nodiscard]] auto GetF() const { return f_; }
+    [[nodiscard]] constexpr TimeT const& GetT() const { return t_; }
+    [[nodiscard]] constexpr ValueT const& GetY() const { return y_; }
+    [[nodiscard]] constexpr auto GetF() const { return f_; }
 
 private:
     TimeT t_;
